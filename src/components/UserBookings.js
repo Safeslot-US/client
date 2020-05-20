0a0f0d3681d37f443cce26b3d0f5123c42cd9cda
christenmartin <christenmartin@gmail.com>
2020-05-20 16:06:04 -0700

    removed some unused vars

--- src/components/UserBookings.js
+++ src/components/UserBookings.js
@@ -2,11 +2,7 @@ import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchUserBookings } from "../store"
 import { useParams } from "react-router-dom";
-import { Global, Container, Item } from '../styles'
 import BookingCard from "./BookingCard";
-import data from '../data'
-import history from "../utils/history";
-const moment = require("moment");
 
 function UserBookings() {
   const userBookings = useSelector(state => state.userBookings);
