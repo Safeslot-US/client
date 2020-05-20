3920a9040667f738014efa2cd38b101d65ed9b6e
christenmartin <christenmartin@gmail.com>
2020-05-19 19:42:17 -0700

    removed slash

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,3 +1,3 @@
 //Temp deployed version that points to my repo. My latest PR needs to be incorporated
 //into Backend-loopback master in order for https://safeslot-server.herokuapp.com/ to work
-export const API_URL = 'https://safeslot-demo-server.herokuapp.com/api/';
+export const API_URL = 'https://safeslot-demo-server.herokuapp.com/api';
